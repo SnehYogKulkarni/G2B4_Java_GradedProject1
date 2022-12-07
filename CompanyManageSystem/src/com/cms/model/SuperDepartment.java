8c7148458ba62d8d18efaf9b7d971444cdcd7230
SnehYogKulkarni <115880442+SnehYogKulkarni@users.noreply.github.com>
2022-12-07 05:55:26 +0530

    Add files via upload

---
+++ CompanyManageSystem/src/com/cms/model/SuperDepartment.java
@@ -0,0 +1,23 @@
+package com.cms.model;
+
+public class SuperDepartment {
+	
+	public String departmentName() {
+		return "Super Department";
+	}
+	
+	public String getTodaysWork() {
+		return "No Work as of now";
+	}
+	
+	public String getWorkDeadline() {
+		return " Nil ";
+	}
+	
+	public static String isTodayAHoliday() {
+		
+		return " Today is not a holiday";
+	}
+	
+
+}
